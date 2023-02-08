753d48ff7b4e8d1284b2854b55c89d500f96a3db
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-02-08 17:16:49 +0000

    more hacking at ce_table!

--- R/ce_table2.R
+++ R/ce_table2.R
@@ -5,16 +5,16 @@
 ce_table2 <- function (he, wtp = 25000, ...)
 {
   data.frame(
-    cost = paste(round(colMeans(he$c)[c(he$ref, he$comp)],2),
+    cost = paste(round(-colMeans(he$c)[c(he$ref, he$comp)],2),
                  apply(he$c, 2,
                        function(x) paste0("(",
-                                          paste(round(quantile(x, probs = c(0.025, 0.975)),2), collapse = ", "),
+                                          paste(round(-quantile(x, probs = c(0.975, 0.025)),2), collapse = ", "),
                                           ")"))[c(he$ref, he$comp)]),
-    eff = paste(round(colMeans(he$e)[c(he$ref,
+    eff = paste(round(-colMeans(he$e)[c(he$ref,
                            he$comp)],3),
     apply(he$e, 2,
                 function(x) paste0("(",
-                                   paste(round(quantile(x, probs = c(0.025, 0.975)),3), collapse = ", "),
+                                   paste(round(-quantile(x, probs = c(0.975, 0.025)),3), collapse = ", "),
                                    ")"))[c(he$ref, he$comp)]),
     delta.c = c(NA, colMeans(he$delta_c)),
     delta.e = c(NA,
