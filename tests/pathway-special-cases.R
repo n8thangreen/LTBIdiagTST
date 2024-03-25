d7eee6eb2f0c47c5dcbb1bc1a893ae270e76e24c
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2024-03-25 11:35:26 +0000

    setting new tree values for comparison tests between dual tree special case and single test trees

--- tests/pathway-special-cases.R
+++ tests/pathway-special-cases.R
@@ -1,4 +1,5 @@
 # test special cases of diagnostic pathways
+# against direectly defined versions
 
 library(dplyr)
 library(reshape2)
@@ -19,15 +20,7 @@ load(here::here("data", "trees.RData"))  #create_trees()
 # markov model starting states
 load("data/state_lists.RData")
 
-
-##################################
-# TST as TST/QFT with perfect TST
-
-
-# set TST parameters
-
-# run TST/QFT decision tree model
-tree_dat <-
+tree_dat_TSPOT <-
   create_ce_tree_long_df(
     tree_list = TSPOT_tree,
     label_probs = label_probs,
@@ -37,11 +30,59 @@ tree_dat <-
     cname_from_to = TSPOT_cname_from_to,
     hname_from_to = TSPOT_hname_from_to)
 
+tree_dat_TST_TSPOT <-
+  create_ce_tree_long_df(
+    tree_list = TST_TSPOT_tree,
+    label_probs = label_probs,
+    label_costs = label_costs,
+    label_health = label_health,
+    pname_from_to = TST_TSPOT_pname_from_to,
+    cname_from_to = TST_TSPOT_cname_from_to,
+    hname_from_to = TST_TSPOT_hname_from_to)
+
+tree_dat_TST <-
+  create_ce_tree_long_df(
+    tree_list = TST_tree,
+    label_probs = label_probs,
+    label_costs = label_costs,
+    label_health = label_health,
+    pname_from_to = TST_pname_from_to,
+    cname_from_to = TST_cname_from_to,
+    hname_from_to = TST_hname_from_to)
+
+########################################
+# TST as TST/QFT with uninformative TST
+
+# TSPOT only as special case of TST/TSPOT
+tree_TSPOT_star <- tree_dat_TST_TSPOT
+
+# set uninformative TST parameter values
+tree_TSPOT_star$prob[tree_TSPOT_star$name.prob == "pAccept_TST"] <- 1
+tree_TSPOT_star$prob[tree_TSPOT_star$name.prob == "pTSTread"] <- 1
+tree_TSPOT_star$prob[tree_TSPOT_star$name.prob == "TST_pos"] <- 1
+tree_TSPOT_star$prob[tree_TSPOT_star$name.prob == "PPV_TST"] <- 1
+tree_TSPOT_star$prob[tree_TSPOT_star$name.prob == "NPV_TST"] <- 0
+
+# match probabilities with TSPOT only decision tree
+tree_TSPOT_star$prob[tree_TSPOT_star$name.prob == "pAccept_IGRA_TST+"] <-
+  tree_dat_TSPOT$prob[tree_dat_TSPOT$name.prob == "pAccept_IGRA"]
+
+tree_TSPOT_star$prob[tree_TSPOT_star$name.prob == "TSPOT_pos_TST+"] <-
+  tree_dat_TSPOT$prob[tree_dat_TSPOT$name.prob == "TSPOT_pos"]
+
+tree_TSPOT_star$prob[tree_TSPOT_star$name.prob == "PPV_TSPOT_TST+"] <-
+  tree_dat_TSPOT$prob[tree_dat_TSPOT$name.prob == "PPV_TSPOT"]
+
+tree_TSPOT_star$prob[tree_TSPOT_star$name.prob == "NPV_TSPOT_TST+"] <-
+  tree_dat_TSPOT$prob[tree_dat_TSPOT$name.prob == "NPV_TSPOT"]
+
+# run TST/QFT decision tree model
+
 # starting states of Markov model
 state_list <- state_lists$TSPOT
 
-dt <-
-  run_cedectree(tree_dat,
+res_TSPOT_star <-
+  run_cedectree(tree_TSPOT_star,
                 label_probs_distns,
                 label_costs_distns,
                 label_health_distns,
@@ -50,32 +91,26 @@ dt <-
 # compare against QFT pathway
 
 # save
-write.csv(tree_dat, file = "testdata/.csv")
+write.csv(tree_TSPOT_star, file = "testdata/tree_TSPOT_star.csv")
 
 
-##################################
-# QFT as TST/QFT with perfect QFT
+########################################
+# QFT as TST/QFT with uninformative QFT
 
-# read QFT pathway parameters
+# TST only as special case of TST/TSPOT
+tree_TST_star <- tree_dat_TST_TSPOT
 
-# set QFT parameters
-
-# run TST/QFT decision tree model
-tree_dat <-
-  create_ce_tree_long_df(
-    tree_list = TSPOT_tree,
-    label_probs = label_probs,
-    label_costs = label_costs,
-    label_health = label_health,
-    pname_from_to = TSPOT_pname_from_to,
-    cname_from_to = TSPOT_cname_from_to,
-    hname_from_to = TSPOT_hname_from_to)
+# set uninformative IGRA parameters
+tree_TST_star$prob[tree_TST_star$name.prob == "pAccept_IGRA_TST+"] <- 1
+tree_TST_star$prob[tree_TST_star$name.prob == "TSPOT_pos_TST+"] <- 1
+tree_TST_star$prob[tree_TST_star$name.prob == "PPV_TSPOT_TST+"] <- 1
+tree_TST_star$prob[tree_TST_star$name.prob == "NPV_TSPOT_TST+"] <- 0
 
 # starting states of Markov model
 state_list <- state_lists$TSPOT
 
-dt <-
-  run_cedectree(tree_dat,
+res_TST_star <-
+  run_cedectree(tree_TST_star,
                 label_probs_distns,
                 label_costs_distns,
                 label_health_distns,
@@ -84,5 +119,5 @@ dt <-
 # compare against TST pathway
 
 # save
-write.csv(tree_dat, file = "testdata/.csv")
+write.csv(tree_TST_star, file = "testdata/tree_TST_star.csv")
 
