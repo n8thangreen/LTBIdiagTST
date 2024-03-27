64628b4e4fc4925c65649eb56ee4220b9b5a3c8b
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2024-03-27 16:30:35 +0000

    special case descision trees now pass tests :)
    * fixed found small error missing Hep cost in one branch of TSPOT and QFT

--- tests/pathway-special-cases.R
+++ tests/pathway-special-cases.R
@@ -63,9 +63,12 @@ tree_TSPOT_star <-
            name.prob == "pAccept_TST" ~ 1,
            name.prob == "pTSTread" ~ 1,
            name.prob == "TST_pos" ~ 1,
-           name.prob == "PPV_TST" ~ 1,
+           name.prob == "PPV_TST" ~
+             filter(tree_dat_TSPOT, name.prob == "pLTBI")$prob,
            name.prob == "NPV_TST" ~ 0,
            # match probabilities with TSPOT only decision tree
+           name.prob == "TSPOT_pos_TST+" ~
+             filter(tree_dat_TSPOT, name.prob == "TSPOT_pos")$prob,
            name.prob == "pAccept_IGRA_TST+" ~
              filter(tree_dat_TSPOT, name.prob == "pAccept_IGRA")$prob,
            name.prob == "PPV_TSPOT_TST+" ~
@@ -82,31 +85,30 @@ tree_TSPOT_star <-
   ) |>
   fill_complementary_probs()
 
-# starting states of Markov model
-state_list <- state_lists$TST_TSPOT
-
 # run TST/QFT decision tree model
+# point value case
 res_TSPOT_star <-
   run_cedectree(dat_long = tree_TSPOT_star,
-                # label_probs_distns,
-                # label_costs_distns,
-                # label_health_distns,
-                state_list = state_list)
+                state_list = state_lists$TST_TSPOT)
 
 # compare against QFT pathway
 
+# point value case
 res_TSPOT <-
-  run_cedectree(dat_long = tree_TSPOT,
-                # label_probs_distns,
-                # label_costs_distns,
-                # label_health_distns,
+  run_cedectree(dat_long = tree_dat_TSPOT,
                 state_list = state_lists$TSPOT)
 
-expect_equivalent(res_TST_star$cost, res_TST$cost)
-expect_equivalent(res_TST_star$health, res_TST$health)
+# tests
+expect_equal(res_TSPOT_star$cost$ev_point[1], res_TSPOT$cost$ev_point[1])
+
+expect_equal(res_TSPOT_star$cost$term_pop_point, res_TSPOT$cost$term_pop_point)
+
+expect_equal(res_TSPOT_star$health$ev_point[1], res_TSPOT$health$ev_point[1], tolerance = 0.00001)
+
+expect_equal(res_TSPOT_star$health$term_pop_point, res_TSPOT$health$term_pop_point)
 
 # save
-write.csv(tree_TSPOT_star, file = "testdata/tree_TSPOT_star.csv")
+write.csv(tree_TSPOT_star, file = "tests/testthat/testdata/tree_TSPOT_star.csv")
 
 
 ########################################
@@ -119,7 +121,8 @@ tree_TST_star <-
          prob = case_when(
            name.prob == "pAccept_IGRA_TST+" ~ 1,
            name.prob == "TSPOT_pos_TST+" ~ 1,
-           name.prob == "PPV_TSPOT_TST+" ~ 1,
+           name.prob == "PPV_TSPOT_TST+" ~
+             filter(tree_dat_TST, name.prob == "PPV_TST")$prob,
            name.prob == "NPV_TSPOT_TST+" ~ 0,
            .default = prob
          ),
@@ -131,25 +134,27 @@ tree_TST_star <-
   fill_complementary_probs()
 
 # run model
+# point value case
 res_TST_star <-
   run_cedectree(dat_long = tree_TST_star,
-                # label_probs_distns,
-                # label_costs_distns,
-                # label_health_distns,
                 state_list = state_lists$TST_TSPOT)
 
 # compare against TST pathway
 
+# point value case
 res_TST <-
-  run_cedectree(dat_long = tree_TST,
-                # label_probs_distns,
-                # label_costs_distns,
-                # label_health_distns,
+  run_cedectree(dat_long = tree_dat_TST,
                 state_list = state_lists$TST)
 
-expect_equivalent(res_TST_star$cost, res_TST$cost)
-expect_equivalent(res_TST_star$health, res_TST$health)
+# tests
+expect_equal(res_TST_star$cost$ev_point[1], res_TST$cost$ev_point[1])
+
+expect_equal(res_TST_star$cost$term_pop_point, res_TST$cost$term_pop_point)
+
+expect_equal(res_TST_star$health$ev_point[1], res_TST$health$ev_point[1])
+
+expect_equal(res_TST_star$health$term_pop_point, res_TST$health$term_pop_point)
 
 # save
-write.csv(tree_TST_star, file = "testdata/tree_TST_star.csv")
+write.csv(tree_TST_star, file = "tests/testthat/testdata/tree_TST_star.csv")
 
